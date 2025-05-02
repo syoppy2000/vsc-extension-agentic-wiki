496f4f48b0cb0d48e0f0074ffb33e916c5e7fcb8
fine <18224017933@163.com>
2025-05-02 18:47:54 +0800

    feat(nodes): CombineTutorialNode

--- src/flow.ts
+++ src/flow.ts
@@ -1,14 +1,27 @@
 import { Flow } from "pocketflow";
 import { SharedStore } from "./types";
-import { AnalyzeRelationshipsNode, FetchRepoNode, IdentifyAbstractionsNode } from "./nodes";
+import {
+    AnalyzeRelationshipsNode,
+    CombineTutorialNode,
+    FetchRepoNode,
+    IdentifyAbstractionsNode,
+    OrderChaptersNode,
+    WriteChaptersNode,
+} from "./nodes";
 
 export function createFlow(): Flow {
     const fetchRepoNode = new FetchRepoNode();
     const identifyAbstractionNode = new IdentifyAbstractionsNode();
     const analyzeRelationshipsNode = new AnalyzeRelationshipsNode();
+    const orderChaptersNode = new OrderChaptersNode();
+    const writeChaptersNode = new WriteChaptersNode();
+    const combineTutorialNode = new CombineTutorialNode();
 
     fetchRepoNode.next(identifyAbstractionNode);
     identifyAbstractionNode.next(analyzeRelationshipsNode);
+    analyzeRelationshipsNode.next(writeChaptersNode);
+    writeChaptersNode.next(orderChaptersNode);
+    orderChaptersNode.next(combineTutorialNode);
 
     return new Flow<SharedStore>(fetchRepoNode);
 }
--- src/nodes/index.ts
+++ src/nodes/index.ts
@@ -2,4 +2,5 @@ export { default as FetchRepoNode } from "./fetchRepoNode";
 export { default as IdentifyAbstractionsNode } from "./identifyAbstractionsNode";
 export { default as AnalyzeRelationshipsNode } from "./analyzeRelationshipsNode";
 export { default as OrderChaptersNode } from "./orderChaptersNode";
-export { default as GenerateChaptersNode } from "./writeChaptersNode";
+export { default as WriteChaptersNode } from "./writeChaptersNode";
+export { default as CombineTutorialNode } from "./combineTutorialNode";
